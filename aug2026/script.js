cc090ac418bdab12bdb9e4d83559f7c692c027d8
helixour <107082084+helixour@users.noreply.github.com>
2026-08-26 16:28:46 -0400

    Day 3

--- aug2026/script.js
+++ aug2026/script.js
@@ -1,3 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
     document.body.style.zoom = "90%"; 
 });
+
+var squishSound = document.getElementById("squishSfx");
+var soundPlayed = false;
+
+function squish() {
+    if (!soundPlayed) {
+        squishSound.play();
+        document.getElementById("raccoon").className = "animateRaccoon";
+        soundPlayed = true;
+    }
+    
+}
+
+function sayMsg() {
+    document.getElementById("btn").innerHTML = "HEY THERE";
+}
