586e498bb0b8bef5906c6ef3cae7bbf27af34073
helixour <107082084+helixour@users.noreply.github.com>
2026-08-27 17:18:33 -0400

    Day 4
    Day 4:
        - Made changelogs.txt
        - Added optimisation changes
        - Made mobile support better
        - Made the code a bit more readable
        - Added raccoon clicker
        - Added mute music button

--- aug2026/cellar/script.js
+++ aug2026/cellar/script.js
@@ -1,3 +1,15 @@
 window.addEventListener('DOMContentLoaded', () => {
     document.body.style.zoom = "90%"; 
 });
+
+var mainMusic = document.getElementById("mainMusic");
+
+function mute() {
+    if (!mainMusic.paused) {
+        mainMusic.pause();
+        document.getElementById("mute").innerHTML = '<img src="../mute.png">'
+    } else {
+        mainMusic.play();
+        document.getElementById("mute").innerHTML = '<img src="../playing.png">'
+    }
+}
